9a4411d22a30cca17d40e28268a21a607e9caa6c
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-02-12 16:10:46 +0100

    Complete exercise 1

--- exercise1/userClass.js
+++ exercise1/userClass.js
@@ -1,20 +1,18 @@
-
-
 function User(name_param, age_param) {
-	this.name = name_param;
-	this.age = age_param;
+  this.name = name_param;
+  this.age = age_param;
 }
 
 User.prototype.compare = function(other_user) {
-	user_age = Number(this.age);
-	other_age = Number(other_user.age);
+  user_age = Number(this.age);
+  other_age = Number(other_user.age);
 
-	if(user_age > other_age) {
-		return '' + this.name + ' is older than ' + other_user.name;
-	} 
-	else if(other_age > user_age) {
-		return '' + other_user.name + ' is older than ' + this.name;
-	}
+  if(user_age > other_age) {
+    return '' + this.name + ' is older than ' + other_user.name;
+  } 
+  else if(other_age > user_age) {
+    return '' + other_user.name + ' is older than ' + this.name;
+  }
 }
 
 var user1 = new User("Joshua", "13");
