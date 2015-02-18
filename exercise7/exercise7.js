5ae8bfdadb8c7d9ba28db1650d40f3a42703d5f2
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-02-18 21:42:22 +0100

    Add solution 7

---
+++ exercise7/exercise7.js
@@ -0,0 +1,22 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+	var fields = ['firstname', 'lastname'], i;
+	var names = []
+
+	for(i = 0; i < fields.length; i++){
+		var currentInput = prompt('Enter your ' + fields[i] , '--your' + fields[i] + ' here--');
+		
+		//converts input to its boolean value (nulll and empty string == false)
+		if (!currentInput){
+			alert('The ' + fields[i] + ' entered is invalid');
+			names = [];
+		} else {
+			names.push(currentInput);
+		}
+	}
+	//if both array was populated wits both names
+	if (names.length == 2) {
+		alert('Hello ' + names.join(', ') );
+		document.body.innerHTML = 'Hello ' + names.join(', ');
+	}
+});
