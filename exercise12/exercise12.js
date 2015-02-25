69c11ead37626ed31582b6a6ee71ea53dd38cefb
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-02-25 13:36:10 +0100

    Complete Solution

---
+++ exercise12/exercise12.js
@@ -0,0 +1,29 @@
+function DomainMatcher(form){
+  this.urlForm = form;
+  that = this;
+}
+
+DomainMatcher.prototype = {
+  extractValues: function(){
+      event.preventDefault();
+      var urlText = that.urlForm['urlText'].value;
+      var regex = /([A-Za-z0-9_\-]{1,}\.)?([A-Za-z0-9_\-]{1,}\.[A-Za-z]{2,3})/ig;
+      result = regex.exec(urlText);
+
+      alert('Domain: ' +  result[2]);
+      if(result[1] != undefined) {
+        alert('Subdomain: ' +  result[1].slice(0, -1));//removes the trailing dot
+      }
+  },
+
+  addEventHandlers: function(){
+    that.urlForm.addEventListener('submit', that.extractValues);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  var form = document.getElementById('urlForm');
+  var matcher = new DomainMatcher(form);
+  matcher.addEventHandlers();
+
+});
