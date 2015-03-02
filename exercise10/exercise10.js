6b46c792c400f263aca2d32689ca467394785d14
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-03-02 11:43:56 +0100

    Update regex and css

--- exercise10/exercise10.js
+++ exercise10/exercise10.js
@@ -53,7 +53,8 @@ FormHandler.prototype = {
   },
 
   validateEmail: function(field) {
-    var emailRegex = /^([a-z][a-z0-9_\-\.])*\@([a-z0-9_\-\.]){1,}\.([a-z]{2,4})$/i;
+    
+    var emailRegex = /^([a-z][a-z\d_\-\.]*)\@([a-z\d_\-]{1,}\.)?([a-z\d_\-]{1,}\.[a-z]{2,3}(\.[a-z]{2})?)$/i;
         
     if(!emailRegex.test(field.value)){
       alert("Please enter a valid email address");
@@ -62,7 +63,7 @@ FormHandler.prototype = {
   },
 
   validateHomepageUrl: function(field) {
-    var homepageRegex = /^(http:\/\/|(www|ww\d)|http:\/\/(www|ww\d))?([A-Za-z0-9_\-\.]){1,}\.[A-Za-z]{2,3}\/?$/;     
+    var homepageRegex = /^((ft|htt)p:\/\/)?(www\.)?([a-z\d_\-]{1,}\.)?([a-z\d_\-]{1,}\.[a-z]{2,3}(\.[a-z]{2})?)$/i     
     if(!homepageRegex.test(field.value)){
       alert("Please enter a valid homepage url");
       return false;
