afd4567ab4724bd1b14cdbd8610f84b73cf91d31
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-03-02 11:26:19 +0100

    Modify regex logic

--- exercise12/exercise12.js
+++ exercise12/exercise12.js
@@ -8,11 +8,7 @@ DomainMatcher.prototype = {
       var urlText = that.urlForm['urlText'].value;
       var regex = /(http:\/\/)?(www\.)?([A-Za-z0-9_\-]{1,}\.)?([A-Za-z0-9_\-]{3,}\.[A-Za-z]{2,3}(\.[A-Za-z]{2})?)/ig;
       var result = regex.exec(urlText);
-      console.log(1 + ': ' + result[1]);
-      console.log(2 + ': ' + result[2]);
-      console.log(3 + ': ' + result[3]);
-      console.log(4 + ': ' + result[4]);
-      console.log(5 + ': ' + result[5])
+
       if(result[3] != undefined) {
         alert('Domain: ' +  result[4] + '\nSubdomain: ' +  result[3].slice(0, -1));//removes the trailing dot
       }
