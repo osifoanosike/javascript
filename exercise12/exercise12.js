f3abc6c34a008e5cdcf20846756920d3d77956b2
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-03-02 14:55:16 +0100

    Optimize regex logic

--- exercise12/exercise12.js
+++ exercise12/exercise12.js
@@ -8,14 +8,19 @@ DomainMatcher.prototype = {
       var urlText = that.urlForm['urlText'].value;
 
       var regex = /^((ft|htt)p:\/\/)?(www\.)?(([a-z\d]+[_\-]?)+)\.(([a-z\d]+[_\-\.]?)(\.[a-z]{2,3}){1,2})$/ig
-      var result = regex.exec(urlText);
-
-      if(result[3] != undefined) {
-        alert('Domain: ' +  result[6] + '\nSubdomain: ' +  result[4]);//removes the trailing dot
+      if(regex.test(urlText)) {
+         var result = regex.exec(urlText);
+        if(result[3] != undefined) {
+          alert('Domain: ' +  result[6] + '\nSubdomain: ' +  result[4]);//removes the trailing dot
+        }
+        else {
+          alert('Domain: ' +  result[6]);
+        }
       }
-      else {
-        alert('Domain: ' +  result[6]);
+      else{
+        alert('The URL you entered is invalid');
       }
+     
   },
 
   addEventHandlers: function(){
