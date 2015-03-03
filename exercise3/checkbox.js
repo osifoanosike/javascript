c4686aaddcbc173795de724d84ca7f3efb74608b
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-03-03 09:11:29 +0100

    Update logic

--- exercise3/checkbox.js
+++ exercise3/checkbox.js
@@ -13,7 +13,7 @@ CheckboxOp.prototype = {
         checkbox = checkBoxes[i]
         checkbox.checked = false;  
       }
-      that.selectedDays = [];
+      this.selectedDays = [];
   },
 
   checkItem: function(currentCheckbox){
@@ -22,28 +22,37 @@ CheckboxOp.prototype = {
       if(this.selectedDays.length < 3 && currentCheckbox.checked == true){         
         this.selectedDays.push(currentCheckbox.id.toString());
         this.noneAction.checked = false
-      } else if (this.selectedDays.length >= 3 && currentCheckbox.checked == true) {
-              
+      } else if(currentCheckbox.checked == true && this.limitReached(this.selectedDays)) {          
+    
         //if array is full and user still tries to add
         currentCheckbox.checked = false;
-        var lastSelected = this.selectedDays[this.selectedDays.length - 1];
-        alert('Only 3 days can be selected.' + 
-          '\nYou have already selected ' + this.selectedDays.slice(0, -1).join(', ') + ' and ' + lastSelected);
-          
-      } else if(currentCheckbox.checked == false){
 
+      } else if(currentCheckbox.checked == false){
         //if the user unchecks...even if array is full
         this.selectedDays.splice(this.selectedDays.indexOf(currentCheckbox.id), 1);
+
       }
     }
   },
 
+  limitReached: function(selectedDays){
+    if (selectedDays.length >= 3) {
+      //currentCheckbox.checked = false;
+      var lastSelected = this.selectedDays[this.selectedDays.length - 1];
+      alert('Only 3 days can be selected.' + 
+        '\nYou have already selected ' + this.selectedDays.slice(0, -1).join(', ') + ' and ' + lastSelected);
+      return true;
+    }
+    else{
+      return false;
+    }        
+  },
 
   addEventListeners: function() {
     var that = this;
 
     this.noneAction.addEventListener('click', function() {
-      var checkBoxes = that.checkboxGroup.querySelectorAll('input[type=checkbox]');
+      var checkBoxes = that.checkboxGroup.getElementsByName('checkbox');
       that.uncheckAll(checkBoxes);
       this.checked = true;  
     });
