cfb9717200237181b03d57fbacd71b2f98d22a5f
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-03-09 10:02:03 +0100

    Optimize and update code

--- exercise3/checkbox.js
+++ exercise3/checkbox.js
@@ -1,9 +1,8 @@
-function CheckboxOp(checkboxGroup, checkBoxes, none_checkbox){
+function CheckboxOp(checkboxGroup, none_checkbox, checkLimit){
   this.checkboxGroup = checkboxGroup;
-  this.checkBoxes = checkBoxes
   this.noneAction = none_checkbox;
   this.selectedDays = [];
-
+  this.checkCountLimit = checkLimit;
 }
 
 
@@ -13,61 +12,51 @@ CheckboxOp.prototype = {
     this.noneAction.checked = true;
   },
 
-  uncheckAll: function(checkBoxes) {
-     for( i = checkBoxes.length; i--; ) {
-        checkbox = checkBoxes[i]
-        checkbox.checked = false;  
-      }
-      this.selectedDays = [];
+  uncheckAll: function() {
+    var checkedItems = this.checkboxGroup.querySelectorAll('.selected');
+    for( i = checkedItems.length; i--; ) {
+      checkedItems[i].checked = false; 
+      checkedItems[i].removeAttribute('class'); 
+    }
+    this.selectedDays = [];
   },
 
   checkItem: function(currentCheckbox){
-    if(currentCheckbox.id != 'none') {
+    this.noneAction.checked = false;
 
-      if(this.selectedDays.length < 3 && currentCheckbox.checked == true){         
-        this.selectedDays.push(currentCheckbox.id.toString());
-        this.noneAction.checked = false
-      } else if(currentCheckbox.checked == true && this.limitReached(this.selectedDays)) {          
-    
-        //if array is full and user still tries to add
-        currentCheckbox.checked = false;
-
-      } else if(currentCheckbox.checked == false){
-        //if the user unchecks...even if array is full
-        this.selectedDays.splice(this.selectedDays.indexOf(currentCheckbox.id), 1);
-
-      }
+    if(this.selectedDays.length < this.checkCountLimit && currentCheckbox.checked){         
+        this.selectedDays.push(currentCheckbox.id);
+        currentCheckbox.setAttribute('class', 'selected');
+    } else if(currentCheckbox.checked  && (this.selectedDays.length >= this.checkCountLimit)) {          
+      this.promptLimitReached();
+      currentCheckbox.checked = false;
+    } else if(!currentCheckbox.checked){
+      currentCheckbox.removeAttribute('class');
+      this.selectedDays.splice(this.selectedDays.indexOf(currentCheckbox.id), 1);
     }
   },
 
-  limitReached: function(selectedDays){
-    if (selectedDays.length >= 3) {
-      //currentCheckbox.checked = false;
-      var lastSelected = this.selectedDays[this.selectedDays.length - 1];
-      alert('Only 3 days can be selected.' + 
-        '\nYou have already selected ' + this.selectedDays.slice(0, -1).join(', ') + ' and ' + lastSelected);
-      return true;
-    }
-    else{
-      return false;
-    }        
+  promptLimitReached: function(){
+    var lastSelected = this.selectedDays[this.selectedDays.length - 1];
+    alert('Only 3 days can be selected.' + 
+      '\nYou have already selected ' + this.selectedDays.slice(0, -1).join(', ') + ' and ' + lastSelected);
   },
 
   addEventListeners: function() {
     var that = this;
 
     this.noneAction.addEventListener('click', function() {
-      if (this.checked == true) {
-        var checkBoxes = that.checkBoxes
-        that.uncheckAll(checkBoxes);
+      if (this.checked) {
+        that.uncheckAll();
         this.checked = true;  
       }   
     });
 
     //delegating all checks to the enclosing section element
     this.checkboxGroup.addEventListener('click', function (e){
-      var currentCheckbox = e.target;
-      that.checkItem(currentCheckbox);  
+      if (e.target.tagName == "INPUT") {
+        that.checkItem(e.target);
+      }      
     });
   }
 }
@@ -75,8 +64,7 @@ CheckboxOp.prototype = {
 document.addEventListener('DOMContentLoaded', function() {
   var checkboxGroup = document.getElementById('checkbox-group');
   var noneAction = document.getElementById('none');
-  var checkBoxes = document.getElementsByName('checkbox');
-  var chkboxOps = new CheckboxOp(checkboxGroup,checkBoxes, noneAction);
+  var chkboxOps = new CheckboxOp(checkboxGroup, noneAction, 3);
   chkboxOps.addEventListeners();
   chkboxOps.intialize()
 });
