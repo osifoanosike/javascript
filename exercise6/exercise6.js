90dd38d2ec22822364cfc4b63920ec0a8a8cd3af
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-03-13 13:26:20 +0100

    Udate code logic

--- exercise6/exercise6.js
+++ exercise6/exercise6.js
@@ -10,7 +10,7 @@ FormHandler.prototype = {
 
     for(var i = 0; i < simple_validation_fields.length; ) {
       var isValid = this.validateFieldInput(simple_validation_fields[i]);
-
+      console.log(isValid)
       if(isValid)
         { i++;}
       else{ break; } 
@@ -31,34 +31,32 @@ FormHandler.prototype = {
     if(!field.value.trim()) {
       alert(field.name + ' can\'t be empty');
       return false;  
-    }
+    }else { return true; }
   },
 
   validateAboutMe: function() {
-    var about_me_field = this.form.elements['About me'], returnVal = true;
+    var about_me_field = this.form.elements['About me'];
     
-    result = this.validateFieldInput(about_me_field)
-    returnVal = (result == false) ? result : returnVal;
+    var result = this.validateFieldInput(about_me_field)
 
-    if (returnVal) {
+    if (result) {
       result = this.validateFieldLength(about_me_field);
-      returnVal = (result == false) ? result : returnVal;
     }
-    return returnVal;
+    return result;
   },
 
   validateFieldLength: function(field) {
     if(field.value.length < 50) {
       alert('Mininum characters allowed for \''+ field.name + '\' is 50');
       return false;
-    }
+    } else { return true; }
   },
 
   validateNotificationCheck: function(){
     if(!this.form.elements['Confirm notifications'].checked) {
       alert('You\'ll need to accept notifications');
       return false;
-    }
+    } else { return true; }
   },
 
   addEventHandlers: function(){
